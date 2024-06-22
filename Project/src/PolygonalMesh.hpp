2b8a06094d3f07c98ae7a950ccbe84f676406057
MARTINASALDUTTI <s293376@studenti.polito.it>
2024-06-22 14:11:02 +0200

    changes

--- Project/src/PolygonalMesh.hpp
+++ Project/src/PolygonalMesh.hpp
@@ -11,13 +11,6 @@ struct PolygonalMesh{
     std::unordered_map<unsigned int, Eigen::Vector3d> coord0DsCellMap;
     std::list<Eigen::Vector3d> Vertices_list;
 
-    /*
-    L'uso di std::find ha una complessità lineare O(n), il che significa che per vettori di grandi dimensioni, questo metodo
-    potrebbe non essere molto efficiente. Se la tua applicazione richiede molte ricerche e inserimenti unici, considerare
-    l'uso di un std::unordered_set<unsigned int> o un std::set<unsigned int>, che offrono inserimenti e ricerche più
-    efficienti (in media O(1) per std::unordered_set e O(log n) per std::set).
-    */
-
     unsigned int Num1DsCell;
     std::list<std::array<Eigen::Vector3d, 2>> edges_list;
     std::unordered_map<unsigned int, std::array<unsigned int, 2>> Cell1DMap;
